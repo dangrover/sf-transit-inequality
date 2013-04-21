c3a6245e32c70ed82f744fee96c4504eb61985f1
Dan Grover <dangrover@Dans-MacBook-Pro.local>
2013-04-21 12:32:08 -0700

    some cleanup

--- code/census_test.py
+++
@@ -1,9 +0,0 @@
-import census
-from util import *
-
-c = census.Census(CENSUS_API_KEY, year=2010)
-
-#result = c.acs.get('B19013_001E', {'for': 'block:60816062003011'})
-result = c.acs.state_county_tract('B19013_001E', '06', '75', '060700')
-print "result = %s" % result
-
