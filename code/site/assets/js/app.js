ba25fdae4007042169379190b87db447711083ab
Michael Belfrage <mike@belfrage.net>
2013-04-21 15:10:05 -0700

    more refactoring

--- code/site/assets/js/app.js
+++ code/site/assets/js/app.js
@@ -27,22 +27,22 @@ angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
         console.log("showing route " + routeId + " for agency " + agencyName);
         $("#graph").html(""); // clear the graph
         // transform the data first
-        var stop_ids = agencyData[agencyName].routes[routeId].stop_ids;
+        var stopIds = agencyData[agencyName].routes[routeId]['stop_ids'];
         var stops = [];
         var incomes = [];
-        $.each(stop_ids, function(index, stop_id) {
-            stop_info = agencyData[agencyName].stops[stop_id];
-            stops.push(stop_info);
-            incomes.push(stop_info.median_income);
+        $.each(stopIds, function(index, stopId) {
+            stopInfo = agencyData[agencyName].stops[stopId];
+            stops.push(stopInfo);
+            incomes.push(stopInfo.median_income);
         });
 
         // dimensions
         var w = 600,
             h = 400,
             margin = 45,
-            dot_r = 8;
+            dotRadius = 8;
         yScale = d3.scale.linear().domain([200000, 0]).range([margin, h - margin]);
-        xScale = d3.scale.linear().domain([0, stop_ids.length]).range([margin, w - margin]);
+        xScale = d3.scale.linear().domain([0, stopIds.length]).range([margin, w - margin]);
         xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(stops.length).tickFormat(function(d, i) {
             if (stops[i]) {
                 return stops[i].name;
@@ -52,11 +52,10 @@ angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
             return "$" + d / 1000 + "K";
         });
 
-
-        money_format = d3.format(",");
-        route_color = agencyData[agencyName].routes[routeId].color;
-        if (route_color == undefined) {
-            route_color = "#000";
+        moneyFormat = d3.format(",");
+        routeColor = agencyData[agencyName].routes[routeId].color;
+        if (routeColor === undefined) {
+            routeColor = "#000";
         }
 
         var tooltip = d3.select("#graph")
@@ -71,7 +70,10 @@ angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
             .style("font-size", "11px")
             .style("box-shadow", "0px 2px 3px rgba(0,0,0,0.5)");
 
-        var svg = d3.select("#graph").append("svg:svg").attr("width", w).attr("height", h + 90)
+        var svg = d3.select("#graph")
+            .append("svg:svg")
+            .attr("width", w)
+            .attr("height", h + 90);
 
         var line = d3.svg.line().interpolate("cardinal").x(function(d, i) {
             return xScale(i);
@@ -79,35 +81,50 @@ angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
             return yScale(d);
         });
 
-        var path = svg.append("svg:path").attr("d", line(incomes)).attr("class", "data-line").attr("stroke", route_color);
+        var path = svg.append("svg:path").attr("d", line(incomes)).attr("class", "data-line").attr("stroke", routeColor);
 
 
         var dots = svg.append("g").selectAll("circle").data(incomes).enter().append("circle").attr("cx", function(d, i) {
             return xScale(i);
         }).attr("cy", function(d, i) {
             return yScale(d);
-        }).attr("r", dot_r).attr("fill-opacity", 0.3).on("mouseover", function(d, i) {
+        }).attr("r", dotRadius).attr("fill-opacity", 0.3).on("mouseover", function(d, i) {
             tooltip.html(function() {
-                return "<strong>" + stops[i].name + "</strong><br/>Median income: $" + money_format(stops[i].median_income) + "<br/>Census Tract: " + stops[i].state_fips + stops[i].county_fips + stops[i].tract_fips;
-            }).style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY) + "px");
+                return "<strong>" + stops[i].name + "</strong><br/>" +
+                    "Median income: $" + moneyFormat(stops[i].median_income) + "<br/>" +
+                    "Census Tract: " + stops[i].state_fips + stops[i].county_fips + stops[i].tract_fips;
+            })
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY) + "px");
 
             tooltip.style("visibility", "visible");
             this.setAttribute("r", "10");
         }).on("mousemove", function() {
-            tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 6) + "px");
+            tooltip
+                .style("top", (event.pageY - 10) + "px")
+                .style("left", (event.pageX + 6) + "px");
         }).on("mouseout", function() {
             tooltip.style("visibility", "hidden");
-            this.setAttribute("r", dot_r);
+            this.setAttribute("r", dotRadius);
         });
 
 
         //Create X axis
-        svg.append("g").attr("class", "axis").attr("transform", "translate(0," + (h - margin) + ")").call(xAxis).selectAll("text").style("text-anchor", "end").attr("dy", "-.5em").attr('dx', "-1em").attr("transform", "rotate(-90)");
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (h - margin) + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dy", "-.5em")
+            .attr('dx', "-1em")
+            .attr("transform", "rotate(-90)");
 
         //Create Y axis
-        svg.append("g").attr("class", "axis").attr("transform", "translate(" + (margin) + ",0)").call(yAxis);
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + (margin) + ",0)")
+            .call(yAxis);
     };
-
-    $scope.hello = "Bello";
 
 }]);
