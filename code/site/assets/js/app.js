b569e3b0c3ba368b2fa9a2645077ae87b4b7e544
Dan Grover <dangrover@Dans-MacBook-Pro.local>
2013-04-21 18:47:22 -0700

    cleaned up display more

--- code/site/assets/js/app.js
+++ code/site/assets/js/app.js
@@ -33,10 +33,11 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
     $scope.displayRoute = function (agencyName, routeId) {
         console.log("showing route " + routeId + " for agency " + agencyName);
         $("#graph").html(""); // clear the graph
+
         // transform the data first
-        var stopIds = agencyData[agencyName].routes[routeId]['stop_ids'];
+        var route = agencyData[agencyName].routes[routeId];
         var stops = [];
-        $.each(stopIds, function(index, stopId) {
+        $.each(route['stop_ids'], function(index, stopId) {
             stops.push(agencyData[agencyName].stops[stopId]);
         });
 
@@ -44,9 +45,9 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
         var w = 600,
             h = 300,
             margin = 45,
-            dotRadius = 8;
+            dotRadius = 5;
         yScale = d3.scale.linear().domain([200000, 0]).range([margin, h - margin]);
-        xScale = d3.scale.linear().domain([0, stopIds.length]).range([margin, w - margin]);
+        xScale = d3.scale.linear().domain([0, stops.length]).range([margin, w - margin]);
         xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(stops.length).tickFormat(function(d, i) {
             if (stops[i]) {
                 return stops[i].name;
@@ -74,10 +75,33 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
             .style("font-size", "11px")
             .style("box-shadow", "0px 2px 3px rgba(0,0,0,0.5)");
 
+        var heading = d3.select("#graph")
+            .append("h4")
+            .html(agencyName+" <small>"+route.name+"</small>");
+
         var svg = d3.select("#graph")
             .append("svg:svg")
             .attr("width", w)
             .attr("height", h + 140);
+
+
+        // X axis
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (h - margin) + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dy", "-.5em")
+            .attr('dx', "-1em")
+            .attr("transform", "rotate(-90)");
+
+        // Y axis
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + (margin) + ",0)")
+            .call(yAxis);
+            
 
         // Data line
         var line = d3.svg.line().interpolate("cardinal").x(function(d, i) {
@@ -89,12 +113,15 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
         var path = svg.append("svg:path").attr("d", line(stops)).attr("class", "data-line").attr("stroke", routeColor);
 
         // Dots for stops
-
-        var dots = svg.append("g").selectAll("circle").data(stops).enter().append("circle").attr("cx", function(d, i) {
+        var dots = svg.append("g").selectAll("circle").data(stops).enter().append("circle")
+        .attr("cx", function(d, i) {
             return xScale(i);
         }).attr("cy", function(d, i) {
             return yScale(d.median_income);
-        }).attr("r", dotRadius).attr("fill-opacity", 0.3).on("mouseover", function(d, i) {
+        }).attr("r", dotRadius)
+        .attr("fill", routeColor)
+        .attr("stroke", "white")
+        .on("mouseover", function(d, i) {
             tooltip.html(function() {
                 return "<strong>" + stops[i].name + "</strong><br/>" +
                     "Median income: $" + moneyFormat(stops[i].median_income) + "<br/>" +
@@ -115,22 +142,6 @@ angular.module('app').controller('AppCtrl', ['$scope', 'DATA_SOURCES', function(
         });
 
 
-        // X axis
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + (h - margin) + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dy", "-.5em")
-            .attr('dx', "-1em")
-            .attr("transform", "rotate(-90)");
-
-        // Y axis
-        svg.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(" + (margin) + ",0)")
-            .call(yAxis);
     };
 
 }]);
